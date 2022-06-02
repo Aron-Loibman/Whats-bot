815d51cf4206904f2b318c2823d69d2ba5731493
Aron <aron.2016204440@unicap.br>
2022-06-02 13:07:00 -0300

    atualização da arvore com categorias

--- DictTree.py
+++ DictTree.py
@@ -1,6 +1,6 @@
 from logging import root
 from platform import node
-
+import json
 from sqlalchemy import null
 
 class Node:
@@ -56,7 +56,7 @@ class Tree:
 
       rootNode = Node(self.root) #criação do nó raiz
   
-      dic_1= {'Aluno Graduação': 1,'Aluno Pós': 2, 'Professor': 3, 'Coordenação': 4, 'Reitoria': 5}
+      dic_1= {'Coordenação': 1,'Estágio': 2, 'Pesquisa': 3, 'Extensão': 4}
       
       NoDic_1 = Node(dic_1) #criação do primeiro nó
       rootNode.setfilhos(NoDic_1) #Primeiro nó se torna filho da raiz
@@ -66,44 +66,39 @@ class Tree:
       #region 2º Nivel, Set filhos e pais, Tree Blocks
 
       #bloco linkando 1.1 a 1
-      dic_1_1= {'Matricula': 1,'Disciplinas': 2, 'Estágio': 3, 'Secretaría': 4, 'Financeiro': 5}
+      dic_1_1= {'Coordenação Geral de Pesquisa': 1,'Coordenação Geral dos Cursos de MBA': 2, 'Coordenação Geral de Estágio – COGEST': 3, 'Coordenadores de Cursos': 4}
       NoDic_1_1 = Node(dic_1_1)
       NoDic_1.setfilhos(NoDic_1_1)
       NoDic_1_1.setpai(NoDic_1)
       
       #bloco linkando 1.2 a 1
-      dic_1_2= {'Mestrado': 1,'Doutorado': 2, 'Católica Business School': 3, 'Especialização': 4}
+      dic_1_2= {'Ainda não estou estagiando': 1,'Já estou estagiando': 2}
       NoDic_1_2 = Node(dic_1_2)
       NoDic_1.setfilhos(NoDic_1_2)
       NoDic_1_2.setpai(NoDic_1)
 
       #bloco linkando 1.3 a 1
-      dic_1_3= {'Burocracia': 1,'Caderneta': 2, 'Calendario letivo': 3}
+      dic_1_3= {'Contato': 1,'Informações': 2, 'Formularios e Documentos': 3}
       NoDic_1_3 = Node(dic_1_3)
       NoDic_1.setfilhos(NoDic_1_3)
       NoDic_1_3.setpai(NoDic_1)
 
       #bloco linkando 1.4 a 1
-      dic_1_4= {'Coordenação Geral de Pesquisa': 1,'Coordenação Geral dos Cursos de MBA': 2, 'Coordenação Geral de Estágio – COGEST': 3, 'Coordenadores de Cursos': 4}
+      dic_1_4= {'Cursos': 1,'Programas e Projetos': 2, 'Como se inscrever num curso': 3, 'Quer ministrar um curso?': 4, 'Fluxograma do processo para criação de cursos': 5}
       NoDic_1_4 = Node(dic_1_4)
       NoDic_1.setfilhos(NoDic_1_4)
       NoDic_1_4.setpai(NoDic_1)
 
-      #bloco linkando 1.5 a 1
-      dic_1_5= {'Reitoria': 1,'Pró-Reitoria Administrativa': 2, 'Pró-Reitoria Comunitária e de Extensão': 3, 'Pró-Reitoria de Graduação': 4, 'Pró-Reitoriade Pesquisa, Pós-graduação e Inovação': 5}
-      NoDic_1_5 = Node(dic_1_5)
-      NoDic_1.setfilhos(NoDic_1_5)
-      NoDic_1_5.setpai(NoDic_1)
       
       #endregion
       
       #region 3º Nivel, Set filhos e pais, Tree Blocks
       #faltam nós de matricula, disciplinas, secretaria e financeiro
      
-      dic_1_1_3= {'Não Estágiando': 1,'Estágio': 2, 'Concedente de Estagio': 3, 'Agentes de integração': 4}
-      NoDic_1_1_3 = Node(dic_1_1_3)
-      NoDic_1_1.setfilhos(NoDic_1_1_3)
-      NoDic_1_1_3.setpai(NoDic_1_1)
+      dic_1_2_1= {'Informações iniciais': 1,'Agencia experimental': 2, 'Agentes de integração': 3, 'Entidades concedentes/oportunidades': 4, 'Regime CLT':5}
+      NoDic_1_2_1 = Node(dic_1_2_1)
+      NoDic_1_1.setfilhos(NoDic_1_2_1)
+      NoDic_1_2_1.setpai(NoDic_1_1)
      
       #endregion
      
@@ -150,11 +145,11 @@ class Tree:
 if __name__ == "__main__":
       #teste
       t = Tree()
-      rootNode.ShowOptions()    
+      #rootNode.ShowOptions()    
       #falta fazer NextOp ser funcional
-      Next = t.NextOp(0)
-      print(Next)
-      
-      
+      #Next = t.NextOp(0)
+      #print(Next)
+
+      print()      
    
       
--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2 import driver
 from selenium import webdriver
 import time
 class WhatsappBot:
